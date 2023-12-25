ec580adcb01a5b4e38d5685c9242babd4b63547e
Claudio Denner <claudiodenner2@gmail.com>
2023-12-25 17:31:48 -0300

    fix: ajust in error's installation TypeORM

--- Backend/backend-sist-car/src/app.module.ts
+++ Backend/backend-sist-car/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientsModule } from './clients/clients.module';
@@ -6,7 +7,21 @@ import { VehiclesModule } from './vehicles/vehicles.module';
 import { SchedulesModule } from './schedules/schedules.module';
 
 @Module({
-  imports: [ClientsModule, VehiclesModule, SchedulesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'admin',
+      database: 'scheduleCar',
+      entities: [],
+      synchronize: true,
+    }),
+    ClientsModule,
+    VehiclesModule,
+    SchedulesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
