d65aeeedb32388f48941244dddde703aa47d8e04
Claudio Denner <claudiodenner2@gmail.com>
2023-12-27 14:29:53 -0300

    feat: implement swagger-ui for docks

--- Backend/backend-sist-car/src/clients/dto/create-client.dto.ts
+++ Backend/backend-sist-car/src/clients/dto/create-client.dto.ts
@@ -1,18 +1,29 @@
 import { IsEmail, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateClientDto {
+  @ApiProperty({
+    description: 'Name complete with even 80 charset',
+    example: 'Claudio Denner',
+  })
   @IsString()
   name_complete: string;
 
+  @ApiProperty()
   @IsString()
   cpf: string;
 
+  @ApiProperty()
   @IsString()
   phone: string;
 
+  /**
+   * List of modules to include in the specification
+   */
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsString()
   gender: string;
 }
--- Backend/backend-sist-car/src/main.ts
+++ Backend/backend-sist-car/src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Documentation API Schedule Revision Car')
+    .setDescription(
+      'In this API, its possible to do schedules and to control dates',
+    )
+    .setVersion('1.0')
+    .addTag('clients')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
